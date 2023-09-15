2e812aa5b2494b018e2702e586d6e0ecb173b4ec
Hiram <116443893+Mvg4m61@users.noreply.github.com>
2023-09-15 14:52:55 +0300

    Add files via upload

---
+++ controllers/Expenses/ExpensesController.js
@@ -0,0 +1,37 @@
+const DataModel = require("../../models/Expenses/ExpensesModel")
+const CreateService = require("../../services/common/CreateService")
+const DeleteService = require("../../services/common/DeleteService")
+const DetailsByIDService = require("../../services/common/DetailsByIDService")
+const ListOneJoinService = require("../../services/common/ListOneJoinService")
+const UpdateService = require("../../services/common/UpdateService")
+
+
+exports.CreateExpenses = async (req, res) => {
+    let Result = await CreateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.UpdateExpenses=async (req, res) => {
+    let Result=await UpdateService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+exports.ExpensesList = async(req, res) => {
+    const SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
+    const SearchArray = [{Note: SearchRgx},{'Type.Name': SearchRgx}]
+    const JoinStage = {$lookup: {from: "expensetypes", localField: "TypeID", foreignField: "_id", as: "Type"}}
+    let Result = await ListOneJoinService(req, DataModel, SearchArray, JoinStage)
+    res.status(200).json(Result)
+}
+
+exports.DeleteExpense = async (req, res) => {
+    let Result = await DeleteService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+exports.ExpenseDetailsByID=async (req, res) => {
+    let Result= await DetailsByIDService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+
---
+++ controllers/Report/ReportController.js
@@ -0,0 +1,27 @@
+const ExpenseReportService = require("../../services/report/ExpenseReportService")
+const PurchasesReportService = require("../../services/report/PurchasesReportService")
+const ReturnReportService = require("../../services/report/ReturnReportService")
+const SalesReportService = require("../../services/report/SalesReportService")
+
+
+
+exports.ExpensesByDate=async (req, res) => {
+    let Result=await ExpenseReportService(req)
+    res.status(200).json(Result)
+}
+
+
+exports.PurchaseByDate=async (req, res) => {
+    let Result=await PurchasesReportService(req)
+    res.status(200).json(Result)
+}
+
+exports.ReturnByDate=async (req, res) => {
+    let Result=await ReturnReportService(req)
+    res.status(200).json(Result)
+}
+
+exports.SalesByDate=async (req, res) => {
+    let Result=await SalesReportService(req)
+    res.status(200).json(Result)
+}
