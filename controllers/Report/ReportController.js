9244e99c988e954edf6738ff359f8a4a9f7c527f
Hiram <116443893+Mvg4m61@users.noreply.github.com>
2023-10-23 22:46:29 +0300

    Update ReportController.js

--- controllers/Report/ReportController.js
+++ controllers/Report/ReportController.js
@@ -21,7 +21,3 @@ exports.ReturnByDate=async (req, res) => {
     res.status(200).json(Result)
 }
 
-exports.SalesByDate=async (req, res) => {
-    let Result=await SalesReportService(req)
-    res.status(200).json(Result)
-}
